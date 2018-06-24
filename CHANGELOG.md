a38da6c9e30ea0fa0ec44d1552563eb385a910bb
Woody Gilk <woody.gilk@gmail.com>
2018-06-23 22:13:00 -0500

    Fork CSV from other payloads
    And switch to league/csv for parsing.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,79 +5,8 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
-## [1.0.0] - 2018-01-25
-
-### Added
-
-- Improved testing and added code coverage reporting
-- Added tests for PHP 7.2
+## [1.0.0] - ???
 
 ### Changed
 
-- Upgraded to the final version of PSR-15 `psr/http-server-middleware`
-
-### Fixed
-
-- Updated license year
-
-## [0.6.0] - 2017-11-13
-
-### Changed
-
-- Replaced `http-interop/http-middleware` with  `http-interop/http-server-middleware`.
-
-### Removed
-
-- Removed support for PHP 5.x.
-
-## [0.5.0] - 2017-09-21
-
-### Changed
-
-- The `contentType()` argument is an array instead a string, allowing to assign multiple values
-- Append `.dist` suffix to phpcs.xml and phpunit.xml files
-- Changed the configuration of phpcs and php_cs
-- Upgraded phpunit to the latest version and improved its config file
-- Updated to `http-interop/http-middleware#0.5`
-
-## [0.4.0] - 2017-02-05
-
-* Added
-  * New option `contentType()` to configure the `Content-Type` request header
-  * Improve CsvPayload
-    - New option `delimiter()` to configure the CSV delimiter character
-    - New option `enclosure()` to configure the CSV enclosure character
-    - New option `escape()` to configure the CSV escape character
-
-* Fixed
-  * CsvPayload: `StreamInterface` fixed left undetached
-
-## [0.3.0] - 2016-12-26
-
-### Changed
-
-- Updated tests
-- Updated to `http-interop/http-middleware#0.4`
-- Updated `friendsofphp/php-cs-fixer#2.0`
-
-## [0.2.0] - 2016-11-27
-
-### Added
-
-- New option `methods()` to configure the allowed methods
-- New option `override()` to configure if the previous parsed body must be overrided
-
-### Changed
-
-- Updated to `http-interop/http-middleware#0.3`
-
-## 0.1.0 - 2016-10-04
-
-First version
-
-[1.0.0]: https://github.com/middlewares/payload/compare/v0.6.0...v1.0.0
-[0.6.0]: https://github.com/middlewares/payload/compare/v0.5.0...v0.6.0
-[0.5.0]: https://github.com/middlewares/payload/compare/v0.4.0...v0.5.0
-[0.4.0]: https://github.com/middlewares/payload/compare/v0.3.0...v0.4.0
-[0.3.0]: https://github.com/middlewares/payload/compare/v0.2.0...v0.3.0
-[0.2.0]: https://github.com/middlewares/payload/compare/v0.1.0...v0.2.0
+- Forked csv payload from payload
---
+++ tests/CsvPayloadTest.php
@@ -0,0 +1,121 @@
+<?php
+declare(strict_types = 1);
+
+namespace Middlewares\Tests;
+
+use InvalidArgumentException;
+use Middlewares\CsvPayload;
+use Middlewares\Utils\Dispatcher;
+use Middlewares\Utils\Factory;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class CsvPayloadTest extends TestCase
+{
+    public function testOptions()
+    {
+        // Mock
+        $payload = (new CsvPayload())
+            ->delimiter('|')
+            ->enclosure('"')
+            ->escape('\\')
+            ->header(0);
+
+        $document = implode("\n", [
+            'Name|Position',
+            '"Adrian Adams"|accountant',
+            '"Fred Franklin"|janitor',
+            '"Sally Smith"|engineer',
+        ]);
+
+        $request = $this->makeRequest('POST', $document);
+
+        // Execute & Verify
+        $this->dispatch($request, [
+            $payload,
+            function (Request $request) {
+                $expected = [
+                    ['Name' => 'Adrian Adams', 'Position' => 'accountant'],
+                    ['Name' => 'Fred Franklin', 'Position' => 'janitor'],
+                    ['Name' => 'Sally Smith', 'Position' => 'engineer'],
+                ];
+                $this->assertSame($expected, $request->getParsedBody());
+            }
+        ]);
+    }
+
+    public function dataInvalidControlCharacter(): array
+    {
+        return [
+            'too long' => ['coucou'],
+            'too short' => [''],
+            'unicode char' => ['💩'],
+            'unicode char PHP7 notation' => ["\u{0001F4A9}"],
+        ];
+    }
+
+    /**
+     * @dataProvider dataInvalidControlCharacter
+     */
+    public function testInvalidDelimiter(string $control)
+    {
+        // Expect
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('delimiter');
+
+        // Execute
+        (new CsvPayload())->delimiter($control);
+    }
+
+    /**
+     * @dataProvider dataInvalidControlCharacter
+     */
+    public function testInvalidEnclosure(string $control)
+    {
+        // Expect
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('enclosure');
+
+        // Execute
+        (new CsvPayload())->enclosure($control);
+    }
+
+    /**
+     * @dataProvider dataInvalidControlCharacter
+     */
+    public function testInvalidEscape(string $control)
+    {
+        // Expect
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('escape');
+
+        // Execute
+        (new CsvPayload())->escape($control);
+    }
+
+    public function testInvalidHeader()
+    {
+        // Expect
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('header');
+
+        // Execute
+        (new CsvPayload())->header(-1);
+    }
+
+    private function makeRequest(string $method, string $body, string $contentType = 'text/csv'): Request
+    {
+        $request = Factory::createServerRequest([], $method)
+            ->withHeader('Content-Type', $contentType);
+
+        $request->getBody()->write($body);
+
+        return $request;
+    }
+
+    private function dispatch(Request $request, array $middleware): Response
+    {
+        return Dispatcher::run($middleware, $request);
+    }
+}
